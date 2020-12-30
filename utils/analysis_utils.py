b3d91ee11169eb7df2643d8cb7328806ac635567
JiangLan2333 <sqsjel@aliyun.com>
2020-12-30 12:51:23 +0800

    add classification

--- utils/analysis_utils.py
+++ utils/analysis_utils.py
@@ -6,14 +6,18 @@
 
 import pickle
 import numpy as np
+import os
+import pandas as pd
 
 import scanpy as sc
 from sklearn.metrics.cluster import adjusted_mutual_info_score
 from sklearn.metrics.cluster import adjusted_rand_score
 from sklearn.metrics.cluster import homogeneity_score
+from sklearn.metrics import cohen_kappa_score, accuracy_score, make_scorer
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_validate
 from sklearn.tree import DecisionTreeClassifier
 
 
@@ -66,8 +70,9 @@ def fetch_low_dimension_data(model_path):
 def classification(features, labels, method="svm", k_fold=5):
     n_label = len(set(labels))
     # initialize classifier
+    model = None
     if method == "svm":
-        model = SVC(decision_function_shape='ovo')
+        model = SVC(decision_function_shape='ovo', kernel='linear', C=1, random_state=42)
     elif method == "knn":
         model = KNeighborsClassifier(n_neighbors=n_label)
     elif method == "rf":
@@ -76,3 +81,36 @@ def classification(features, labels, method="svm", k_fold=5):
         model = DecisionTreeClassifier(random_state=0)
 
     # train and eval model
+    scoring = {"acc": make_scorer(accuracy_score), "kappa": make_scorer(cohen_kappa_score)}
+    scores = cross_validate(model, features, labels, cv=k_fold, scoring=scoring)
+    return scores['test_acc'], scores['test_kappa']
+
+
+def run_classification():
+    cluster = pd.DataFrame(columns=("task", 'method', 'acc', 'kappa', 'k-fold'))
+    for task in ["gmvshl"]:
+        aux_matrix_file = os.path.join("../aux_files", task, "sc_matrix.pickle")
+        with open(aux_matrix_file, "rb") as f:
+            sc_matrix = pickle.load(f)
+        # load data
+        sc_H = np.loadtxt(os.path.join("../result", task, "sc-only", "sc_H.out"), delimiter=",", dtype=float)
+        labels = np.loadtxt(os.path.join("../result", task, "sc-only", "sc_labels.out"), delimiter=",", dtype=str)
+        # analyse H
+        acc, kappa = classification(sc_H.T, labels, method="svm", k_fold=5)
+        for k in range(5):
+            cluster = cluster.append([{'task': task, 'method': "H_svm", "acc": acc[k],
+                                       "kappa": kappa[k], "k-fold": k + 1}], ignore_index=True)
+        print(cluster)
+        # analyse X
+        for method in ["svm", "knn", "rf", "dtree"]:
+            acc, kappa = classification(sc_matrix.T, labels, method=method, k_fold=5)
+            for k in range(5):
+                cluster = cluster.append([{'task': task, 'method': "X_" + method, "acc": acc[k],
+                                           "kappa": kappa[k], "k-fold": k + 1}], ignore_index=True)
+            print(cluster)
+        return cluster
+
+
+if __name__ == "__main__":
+    cls_result = run_classification()
+    cls_result.to_csv("../result/gmvshl_result.csv")
